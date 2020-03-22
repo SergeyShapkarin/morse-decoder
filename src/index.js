9652dad5d62e711c839d73eccdc0c8e8b2c7f0e7
Sergey Shapkarin <Sergey.Shapkarin@mail.ru>
2020-03-23 03:44:32 +0600

    fix: src/index.js

--- src/index.js
+++ src/index.js
@@ -38,8 +38,42 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+  result = [];
+  
+  let charStart = 0;
+  let charStep = 10;
+  let char = "";
+  
+    while (charStart <= expr.length) {
+      let codeStart = 0;
+      let codeStep = 2;
+      let code = '';
+      char = expr.substr(charStart, charStep);
+      let charCode = [];
+        while (codeStart <= char.length) {
+         let code = char.substr(codeStart, codeStep);
+            if (code === "10") {
+              charCode.push(...".");
+            }
+            if (code === "11") {
+              charCode.push(..."-");
+            }
+            if (code === "**") {
+              charCode.push(..." ");
+            }
+          codeStart += codeStep;
+        }
+      charCode = charCode.join("");
+      if ( charCode === "     " ) {
+        result.push(" ");
+      } else {
+        result.push(MORSE_TABLE[charCode]);
+      }
+      charStart += charStep;
+    }
+  return result.join("");
 }
+
 
 module.exports = {
     decode
